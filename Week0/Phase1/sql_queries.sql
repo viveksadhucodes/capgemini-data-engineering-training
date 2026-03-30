591230c250237c86f00334c1fffd741d18b0d5c0
viveksadhucodes <viveksadhu.work@gmail.com>
2026-03-30 15:53:05 +0530

    Create sql_queries.sql

---
+++ Week0/Phase1/sql_queries.sql
@@ -0,0 +1,33 @@
+CREATE TABLE customers (
+    customer_id   INT,
+    customer_name VARCHAR(50),
+    city          VARCHAR(50),
+    age           INT
+);
+
+
+INSERT INTO customers VALUES
+(1, 'Ravi',  'Hyderabad', 25),
+(2, 'Sita',  'Chennai',   32),
+(3, 'Arun',  'Hyderabad', 28),
+(4, 'Meena', 'Bengaluru', 35),
+(5, 'Kiran', 'Chennai',   22);
+
+SELECT * FROM customers;
+
+SELECT * FROM customers
+WHERE city = 'Chennai';
+
+
+SELECT * FROM customers
+WHERE age > 25;
+
+
+SELECT customer_name, city
+FROM customers;
+
+
+SELECT city, COUNT(*) AS customer_count
+FROM customers
+GROUP BY city
+ORDER BY customer_count DESC;
