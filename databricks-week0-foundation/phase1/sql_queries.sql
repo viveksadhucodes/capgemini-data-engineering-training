1284040d075bbcff3843ab1cc59035a796bd2b1d
viveksadhucodes <viveksadhu.work@gmail.com>
2026-03-30 15:13:49 +0530

    Delete databricks-week0-foundation directory

--- databricks-week0-foundation/phase1/sql_queries.sql
+++
@@ -1,9 +0,0 @@
-SELECT * FROM customers;
-
-SELECT * FROM customers WHERE city='Chennai';
-
-SELECT * FROM customers WHERE age > 25;
-
-SELECT customer_name, city FROM customers;
-
-SELECT city, COUNT(*) FROM customers GROUP BY city;
