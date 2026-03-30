65989c57231fda88eff334110f7c653586e559d2
viveksadhucodes <viveksadhu.work@gmail.com>
2026-03-30 11:47:21 +0530

    Update sql_queries.sql

--- databricks-week0-foundation/phase1/sql_queries.sql
+++ databricks-week0-foundation/phase1/sql_queries.sql
@@ -1,1 +1,9 @@
+SELECT * FROM customers;
 
+SELECT * FROM customers WHERE city='Chennai';
+
+SELECT * FROM customers WHERE age > 25;
+
+SELECT customer_name, city FROM customers;
+
+SELECT city, COUNT(*) FROM customers GROUP BY city;
