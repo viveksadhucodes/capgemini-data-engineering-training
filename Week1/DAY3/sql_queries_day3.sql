f714c0638203ca30bd1ab0170ea4f6e293f63f20
VIVEKSADHU <viveksadhu369@gmail.com>
2026-04-08 12:40:12 +0530

    Add SQL queries for employee row numbering based on salary, joining date, and department

--- Week1/DAY3/sql_queries_day3.sql
+++ Week1/DAY3/sql_queries_day3.sql
@@ -1,3 +1,38 @@
+CREATE TABLE Employee (
+emp_id INT,
+emp_name VARCHAR(50),
+department VARCHAR(50),
+salary INT,
+experience INT,
+performance_rating CHAR(1)
+);
+INSERT INTO Employee (emp_id, emp_name, department, salary, experience, performance_rating)
+VALUES
+(1, 'Karthik', 'Engineering', 95000, 9, 'A'),
+(2, 'Prathik', 'HR', 55000, 4, 'B'),
+(3, 'Vinay', 'Finance', 78000, 10, 'B'),
+(4, 'Vijay', 'Marketing', 48000, 3, 'C'),
+(5, 'Anil', 'Engineering', 88000, 6, 'A'),
+(6, 'Suresh', 'Finance', 92000, 12, 'A'),
+(7, 'Ramesh', 'HR', 46000, 2, 'C'),
+(8, 'Mahesh', 'Marketing', 67000, 7, 'B'),
+(9, 'Rajesh', 'Engineering', 72000, 5, 'B'),
+(10,'Naveen', 'Finance', 61000, 6, 'C'),
+(11,'Deepak', 'HR', 83000, 11, 'A'),
+(12,'Arjun', 'Engineering', 54000, 3, 'C'),
+(13,'Kiran', 'Marketing', 76000, 8, 'A'),
+(14,'Rohit', 'Finance', 68000, 4, 'B'),
+(15,'Pavan', 'HR', 59000, 5, 'B'),
+(16,'Srikanth', 'Engineering', 102000, 14, 'A'),
+(17,'Manoj', 'Finance', 47000, 2, 'C'),
+(18,'Varun', 'Marketing', 52000, 6, 'B'),
+(19,'Ashok', 'HR', 74000, 9, 'A'),
+(20,'Sunil', 'Engineering', 66000, 4, 'B'),
+(21,'Nikhil', 'Finance', 86000, 7, 'A'),
+(22,'Harish', 'Marketing', 45000, 1, 'C'),
+(23,'Vamsi', 'Engineering', 79000, 8, 'B'),
+(24,'Chaitanya','HR', 91000, 13, 'A'),
+(25,'Lokesh', 'Finance', 58000, 5, 'B');
 
 SELECT *,
 	CASE 
---
+++ Week1/DAY3/sql_window_functions.sql
@@ -0,0 +1,38 @@
+-- 1.	Assign a unique row number to all employees based on salary (highest first). 
+SELECT 
+    *, 
+    ROW_NUMBER() OVER (ORDER BY salary DESC) as row_num
+FROM employees;
+
+
+-- 2.	Assign row numbers to employees within each department based on salary descending. 
+SELECT *,ROW_NUMBER()  OVER (PARTITION BY department ORDER BY salary DESC) AS salray_desc_dept
+FROM employees ;
+
+
+-- 3.	Assign row numbers based on employee joining date (latest first). 
+SELECT 
+    *, 
+    ROW_NUMBER() OVER (ORDER BY join_date DESC) as row_num
+FROM employees;
+
+
+-- 4.Assign row numbers within each department based on earliest joining date.  
+SELECT *,ROW_NUMBER()  OVER (PARTITION BY department ORDER BY join_date DESC) AS salray_desc_dept
+FROM employees ;
+
+
+-- 5.Assign row numbers to orders based on order date.  
+
+
+
+-- 7.Assign row numbers to employees based on salary (lowest first). 
+SELECT 
+    *, 
+    ROW_NUMBER() OVER (ORDER BY salary ASC) as row_num
+FROM employees;
+ 
+ 
+-- 8.	Assign row numbers within department for employees based on name alphabetically. 
+SELECT *,ROW_NUMBER() OVER(PARTITION BY department ORDER BY emp_name ASC ) AS dept_wise_names_order
+FROM employees ;
