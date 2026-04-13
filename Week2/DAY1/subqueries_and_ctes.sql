6f43ee7110c4025f2d4c6dfff13ca0140133b7fb
viveksadhucodes <viveksadhu.work@gmail.com>
2026-04-13 15:22:34 +0530

    docs(sql): update Week2 Day1 subqueries and CTEs notes

--- Week2/DAY1/subqueries_and_ctes.sql
+++ Week2/DAY1/subqueries_and_ctes.sql
@@ -59,3 +59,66 @@ ORDER BY name;
 SELECT name
 FROM employees
 WHERE salary > ( SELECT MAX(salary)  FROM employees  WHERE department_id=102 )
+
+
+-- 1 Create a CTE to show employees with salary > 50,000 and fetch all records
+WITH HighEarners AS (
+    SELECT * FROM employees 
+    WHERE salary > 50000
+)
+SELECT * FROM HighEarners;
+
+-- 2 Find average salary per department using CTE
+WITH dept_avg AS (
+    SELECT department_id, AVG(salary) AS average_salary
+    FROM employees
+    GROUP BY department_id
+)
+SELECT d.dept_name, a.average_salary
+FROM Departments d
+JOIN dept_avg a ON d.dept_id = a.department_id;
+
+-- 3 Use CTE to get employees and their department names
+WITH EmpDept AS (
+    SELECT 
+        e.name AS employee_name, 
+        d.dept_name AS department
+    FROM employees e
+    JOIN Departments d ON e.department_id = d.dept_id
+)
+SELECT * FROM EmpDept;
+
+
+-- 4 Find total order amount per employee using CTE
+WITH total_order AS (
+    SELECT 
+        emp_id, 
+        SUM(order_amount) AS total_amount
+    FROM Orders
+    GROUP BY emp_id
+)
+SELECT 
+    e.name, 
+    COALESCE(t.total_amount, 0) AS total_sales
+FROM employees e
+LEFT JOIN total_order t ON e.emp_id = t.emp_id;
+
+
+-- 5 Find employees whose salary is above department average using CTE
+-- 5 Find employees whose salary is above department average using CTE
+WITH DeptAverages AS (
+    SELECT 
+        department_id, 
+        AVG(salary) AS avg_dept_salary
+    FROM employees
+    GROUP BY department_id
+)
+SELECT 
+    e.name, 
+    e.salary, 
+    d.dept_name,
+    ROUND(da.avg_dept_salary, 2) AS dept_average
+FROM employees e
+JOIN DeptAverages da ON e.department_id = da.department_id
+JOIN Departments d ON e.department_id = d.dept_id
+WHERE e.salary > da.avg_dept_salary;
